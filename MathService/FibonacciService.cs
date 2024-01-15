9a523a87656137948a4fe64ac5c79c7578ab06f6
Leah Casey <alcasey@bsu.edu>
2024-01-15 17:18:44 -0500

    Initial commit

---
+++ MathService/FibonacciService.cs
@@ -0,0 +1,13 @@
+﻿namespace MathService;
+
+public class FibonacciService
+{
+    public static int GetFibonacciValueOf(int n)
+    {
+        if (n == 1 || n == 0)
+        {
+            return n;
+        }
+        return GetFibonacciValueOf(n - 1) + GetFibonacciValueOf(n - 2);
+    }
+}
