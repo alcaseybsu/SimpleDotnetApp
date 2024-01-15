e38b0e30d9c7a20a4ffd27b7ce560924a2284a50
Leah Casey <alcasey@bsu.edu>
2024-01-15 17:58:27 -0500

    Update Program.cs

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,6 +1,7 @@
+using MathService;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/", (int value) => "Hello CS420! Here is Fib("+value+")= 
+"+FibonacciService.GetFibonacciValueOf(value));
 app.Run();
+
